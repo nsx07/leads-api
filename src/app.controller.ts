ae47567e5ab10a1f14cc8c0bde09a6bcf30ed344
nsx07 <felipeaugustodluz@hotmail.com>
2024-06-23 20:28:53 -0300

    adding scripts and echo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('say/:something')
-  getTest(@Param('something') word: string): string {
-    return word;
+  @Get('echo')
+  getEcho(): string {
+    return this.appService.getEcho();
   }
 }
